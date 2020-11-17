670b3d625484f4da90ab0eca872667650ee3e482
cubuspl42 <cubuspl42@gmail.com>
2020-11-17 21:07:53 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+	devServer: {
+		contentBase: __dirname,
+		port: 3035,
+		publicPath: '/',
+	},
+};
