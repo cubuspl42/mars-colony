d99e51786fb418f17ec16ebd955a4934d17c49ee
cubuspl42 <cubuspl42@gmail.com>
2020-11-19 20:33:45 +0100

    Draw yellow hex

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import * as tm from 'transformation-matrix';
 const x0 = 0;
 const y0 = 0;
 
+class MyColors {
+    static readonly ground = "#c87137";
+    static readonly hexBorder = "black";
+    static readonly selectedHexBorder = "yellow";
+}
+
 interface Vec2 {
     readonly x: number;
     readonly y: number;
@@ -30,6 +36,8 @@ function drawHex(
         { x: x - a / 2, y: y + h },
     ].map((p) => tm.applyToPoint(matrix, p));
 
+    ctx.beginPath();
+
     const p0 = points[points.length - 1];
     ctx.moveTo(p0.x, p0.y);
 
@@ -38,18 +46,24 @@ function drawHex(
     });
 
     ctx.stroke();
+
+    ctx.closePath();
 }
 
 const canvas = createFullscreenCanvas((ctx) => {
     const canvas = ctx.canvas;
 
-    ctx.fillStyle = "#c87137";
+    ctx.fillStyle = MyColors.ground;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.lineWidth = 2;
 
     const matrix: tm.Matrix = tm.compose(
         tm.translate(canvas.width / 2, canvas.height / 2),
         tm.scale(1, 3 / 4),
     );
+
+    ctx.strokeStyle = MyColors.hexBorder;
 
     const n = 4;
     for (let i = -n; i <= n; ++i) {
@@ -57,6 +71,10 @@ const canvas = createFullscreenCanvas((ctx) => {
             drawHex(ctx, matrix, i, j);
         }
     }
+
+    ctx.strokeStyle = MyColors.selectedHexBorder;
+
+    drawHex(ctx, matrix, 0, 0);
 });
 
 document.body.appendChild(canvas);
