d662db93df4e66b617f2bd6e5c3dcea31a7e1f44
cubuspl42 <cubuspl42@gmail.com>
2020-12-04 22:24:58 +0100

    Make common code work on the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,10 @@
 import * as http from "http";
+import { MutableCell } from "@common/frp/Cell";
+
+
+const cell = new MutableCell(42);
+
+console.log(`Hello world! ${cell.value}`);
 
 const server = http.createServer((req, res) => {
     res.writeHead(200);
