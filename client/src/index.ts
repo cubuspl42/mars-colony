b6d1841c49d038459ed2177437b4618d626c6fe6
cubuspl42 <cubuspl42@gmail.com>
2020-12-16 22:12:15 +0100

    WIP

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,22 +1,172 @@
 import { MutableCell } from "@common/frp/Cell";
 import { createHexGridCanvas } from "./drawing";
-import { HexCoord } from "@common/game/game";
+import { Game, HexCoord } from "@common/game/game";
 import { createBuildingGroup } from "./buildings_group";
 import { createHudElement } from "./hud";
 import { ClientGame } from "./game/game";
+import { Stream, StreamSink } from "@common/frp/Stream";
 
-async function main() {
-    const game = await ClientGame.connect();
+interface ButtonElement {
+    readonly element: HTMLElement;
+    readonly onPressed: Stream<null>;
+}
 
+function createElement<K extends keyof HTMLElementTagNameMap>(
+    tagName: K,
+    args: {
+        readonly init?: (style: CSSStyleDeclaration, element: HTMLElementTagNameMap[K]) => void,
+        readonly children?: ReadonlyArray<Node>,
+    },
+): HTMLElementTagNameMap[K] {
+    const element = document.createElement(tagName);
+
+    const init = args?.init;
+
+    if (init !== undefined) {
+        init(element.style, element);
+    }
+
+    args?.children?.forEach((child) => {
+        element.appendChild(child);
+    });
+
+    return element;
+}
+
+function createButtonElement(args: {
+    readonly init?: (style: CSSStyleDeclaration, element: HTMLButtonElement) => void,
+    readonly text: string,
+},): ButtonElement {
+    const onPressed = new StreamSink<null>();
+
+    const element = createElement("button", {
+        init: (style, element) => {
+            element.addEventListener("click", () => {
+                onPressed.send(null);
+            });
+
+            const init = args?.init;
+
+            if (init !== undefined) {
+                init(style, element);
+            }
+
+            element.innerText = args.text;
+        },
+    })
+
+    return {
+        element,
+        onPressed,
+    };
+}
+
+enum LoginState {
+    IDLE,
+    LOGGING_IN,
+}
+
+enum LoginError {
+    INCORRECT_USERNAME_PASSWORD,
+}
+
+function createLoginView(): HTMLElement {
+    const submitButton = createButtonElement({
+        init: (style, element) => {
+            style.marginTop = "10px";
+            style.padding = "10px";
+        },
+        text: "Sign in!",
+    });
+
+    const state = new MutableCell(LoginState.IDLE);
+
+
+
+    const error = new MutableCell<LoginError | undefined>(undefined);
+
+    const createLabeledTextInput = (args: {
+        readonly labelTex: string,
+    }) =>
+        createElement("div", {
+            init: (style, element) => {
+                style.width = "100%";
+                style.display = "flex";
+                style.flexDirection = "column";
+                style.marginBottom = `10px`;
+            },
+            children: [
+                createElement("label", {
+                    init: (style, element) => {
+                        element.innerText = args.labelTex;
+                        style.marginBottom = `5px`;
+                    },
+                }),
+                createElement("input", {
+                    init: (style, element) => {
+                        element.type = "text";
+                    },
+                }),
+            ],
+        });
+
+    return createElement("div", {
+        init: (style, element) => {
+            style.width = "100vw";
+            style.height = "100vh";
+            style.backgroundColor = "#d4d4d4";
+
+            style.display = "flex";
+            style.justifyContent = "center";
+            style.alignItems = "center";
+        },
+        children: [
+            createElement("div", {
+                init: (style, element) => {
+                    style.width = "200px";
+                    style.padding = "20px";
+
+                    style.backgroundColor = "#8d8d8d";
+
+                    style.display = "flex";
+                    style.flexDirection = "column";
+                    style.justifyContent = "center";
+                    style.alignItems = "center";
+                },
+                children: [
+                    createLabeledTextInput({
+                        labelTex: "Username",
+                    }),
+                    createLabeledTextInput({
+                        labelTex: "Password",
+                    }),
+                    createElement("button", {
+                        init: (style, element) => {
+                            element.innerText = "Log in";
+
+                            element.onclick = () => {
+
+                            };
+
+                            style.marginTop = "10px";
+                            style.padding = "10px";
+                        }
+                    }),
+                    submitButton.element,
+                ]
+            }),
+        ]
+    });
+}
+
+function createGameView(game: Game): HTMLElement {
     const selectedHexCoord = new MutableCell<HexCoord>({ i: 0, j: 0 });
 
-    const root = document.createElement("div");
+    const gameView = document.createElement("div");
 
-    root.style.position = "relative";
-    root.style.width = "100vw";
-    root.style.height = "100vh";
-
-    document.body.appendChild(root);
+    gameView.style.position = "relative";
+    gameView.style.width = "100vw";
+    gameView.style.height = "100vh";
 
     const canvas = createHexGridCanvas({
         selectedHexCoord: selectedHexCoord,
@@ -29,15 +179,27 @@ async function main() {
     canvas.style.left = "0";
     canvas.style.right = "0";
 
-    root.appendChild(canvas);
+    gameView.appendChild(canvas);
 
     const buildingsGroup = createBuildingGroup({ game });
 
-    root.appendChild(buildingsGroup);
+    gameView.appendChild(buildingsGroup);
 
     const hud = createHudElement({ game });
 
-    root.appendChild(hud);
+    gameView.appendChild(hud);
+
+    return gameView;
+}
+
+async function main() {
+    const loginView = createLoginView();
+
+    const game = await ClientGame.connect();
+
+    const gameView = createGameView(game);
+
+    document.body.appendChild(loginView);
 }
 
 main();
